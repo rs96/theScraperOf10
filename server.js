e78d9af55f16d7a3b334af5b806e7fd55bc7e182
Robert Shipley <shipleymrobert@gmail.com>
2022-02-11 12:18:38 +0000

    updated server so it works

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
+const main = require("./main");
 
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Server Root"));
 
-app.get("/athlete/:id", (req, res) => {});
+app.get("/athlete/:id", async (req, res) => {
+    const athleteData = await main
+        .getAthleteData(req.params.id)
+        .then((result) => result)
+        .catch((error) => error);
+    res.send(athleteData);
+});
 
 app.listen(port, () =>
-    console.log(`Tracked server is running at http://localhost:${port}`)
+    console.log(`theScraperOf10 is running at http://localhost:${port}`)
 );
